90e1bb0f0a44a96cb59674b760edb8e3237cdb4a
Weronika Naziemiec <wnaziemiec@gmail.com>
2018-10-08 22:00:34 +0200

    points on the map

--- CardioCarta/Controllers/MapController.cs
+++ CardioCarta/Controllers/MapController.cs
@@ -13,5 +13,23 @@ namespace CardioCarta.Controllers
         {
             return View();
         }
+
+        //GET: Data
+        public List<string> GetSpatialData()
+        {
+            List<string> spatialDataList = new List<string>();
+            //dbConnection.Open();
+            //var command = new SqlCommand("select [GEOMETRY].STAsText() from TEST_SPATIAL where GEOMETRY IS NOT NULL", dbConnection);
+            //var rdr = command.ExecuteReader();
+            //while (rdr.Read())
+            //{
+            //    spatialDataList.Add(Convert.ToString(rdr[0]));
+            //}
+            //dbConnection.Close();
+
+            //spatialDataList.Add("POINT(50.062006 19.940984)");
+            spatialDataList.Add("LINESTRING(1 2 3, 3 4 5)");
+            return spatialDataList;
+        }
     }
 }
